06113d64cb027de9ffc51ef0a2f545ab46fdffa0
Silvia <silvia.tulli.92@gmail.com>
2023-04-16 23:06:42 +0200

    adding database and multi-linear regression

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
 import pickle
 
+
 app = Flask(__name__, template_folder='templates')
 
+
 # Load the trained machine learning model
-with open('regression.pkl', 'rb') as file:
-    regression = pickle.load(file)
+with open('multi_regression.pkl', 'rb') as file:
+    multi_regression = pickle.load(file)
 
 # Define a route for the home page
 @app.route('/')
@@ -14,15 +16,21 @@ def home():
 # Define a route for the home page
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
+	# Get input data from request
     prediction = 0
     if request.method == 'POST':
         # Get the user's mood from the form
+        input_data = request.form.get("input_data")
         mood = request.form['mood']
+        sleep = request.form['sleep']
         try:
             # Convert the input to a numeric format
             mood = float(mood)
+            sleep = float(sleep)
             # Use the machine learning model to make a prediction
-            prediction = regression.predict([[mood]])
+            prediction = multi_regression.predict([[mood, sleep]])
+
+            #prediction = multi_regression.predict(input_data)
             # Display the prediction on the page
             return render_template('result.html', prediction=prediction)
         except ValueError:
--- model_training.py
+++ model_training.py
@@ -1,12 +1,28 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import pickle
+from pymongo import MongoClient
+
+# Initialize MongoDB
+client = MongoClient('mongodb+srv://silvia:polpette@cluster0.7rjc0ph.mongodb.net/?retryWrites=true&w=majority')
+db = client['data']
+collection = db['test.users']
+
+
+
+
+data = collection.find({})
+
+
+# Insert data
+# data = {'mood': [0,1,2,3,4,5,0,1,2,3,4,5], 'sleep': [7,9,10,6,10,8,7,9,10,6,5,8], 'work_hours': [3,4,10,6,7,8,3,4,5,8,10,8]}
+# db.users.insert_one(data)
 
 # Load the data from the CSV file
 data = pd.read_csv('data.csv')
 
 # Split the data into input (X) and output (y) variables
-X = data[['mood']]
+X = data[['mood', 'sleep']]
 y = data['work_hours']
 
 # Train a linear regression model on the data
@@ -16,7 +32,7 @@ model.fit(X, y)
 y_pred = model.predict(X)
 print(y_pred)
 # Save the trained model to a file using pickle
-with open('regression.pkl', 'wb') as file:
+with open('multi_regression.pkl', 'wb') as file:
     pickle.dump(model, file)
 
-print("Model trained and saved as regression.pkl")
+print("Model trained and saved as multi_regression.pkl")
